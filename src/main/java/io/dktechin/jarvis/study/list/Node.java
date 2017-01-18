49329097e4eaf4d4c52d6eee6146b44076f28e35
jarvis <jarvis88@daumcorp.com>
2017-01-18 22:34:04 +0900

    linked list

---
+++ src/main/java/io/dktechin/jarvis/study/list/Node.java
@@ -0,0 +1,18 @@
+package io.dktechin.jarvis.study.list;
+
+public class Node<T> {
+
+    T value;
+
+    Node next;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public Node(T value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+}
