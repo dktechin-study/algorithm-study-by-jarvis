01b1d85b85db0ac3696da3309cde44523bf7319c
jarvis <jarvis88@daumcorp.com>
2017-01-26 12:07:03 +0900

    유클리드 호제법 1인경우 수정

--- src/main/java/io/dktechin/jarvis/study/test/GreatestCommonDivisor.java
+++ src/main/java/io/dktechin/jarvis/study/test/GreatestCommonDivisor.java
@@ -1,6 +1,22 @@
 package io.dktechin.jarvis.study.test;
 
-public class Gcd {
+public class GreatestCommonDivisor {
 
-    public static int
+    public static int gcd(int value1, int value2) {
+        if ( value1 == 0 ) {
+            return value2;
+        }
+
+        if ( value1 == 1 ) {
+            return -1;
+        }
+
+        return gcd(value2 % value1, value1);
+    }
+
+    public static final void main(String[] args) {
+        System.out.println(gcd(80, 100));
+        System.out.println(gcd(12, 36));
+        System.out.println(gcd(12, 35));
+    }
 }
