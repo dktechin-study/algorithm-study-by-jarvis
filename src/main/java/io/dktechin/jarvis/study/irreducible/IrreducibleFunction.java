b71f7c9cdb83ad64aea5b30e00788190b8077388
jarvis <jarvis88@daumcorp.com>
2017-02-17 19:16:54 +0900

    기약분수 문제.

---
+++ src/main/java/io/dktechin/jarvis/study/irreducible/IrreducibleFunction.java
@@ -0,0 +1,27 @@
+package io.dktechin.jarvis.study.irreducible;
+
+/**
+ * 분할정복법으로 기약분수 구하기
+ * 입력
+ * n : n값 이하의 기약분수 전부 구하기
+ *
+ * 궁금점 : 분할정복 챕터에 기약분수 문제가 있는데, 이 문제를 분할정복법으로 풀수 있나??
+ */
+public class IrreducibleFunction {
+
+    public void solve(int n) {
+        for ( int s = 0; s < n; s++ ) {
+
+        }
+    }
+
+    public void solve(int a, int b) {
+
+    }
+
+
+    public boolean gcd(int a, int b) {
+
+        return true;
+    }
+}
