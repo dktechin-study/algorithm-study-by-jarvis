6bb60c0279bc362e8f05ca9ac6d9f7a3b8329bb9
jarvis <jarvis88@daumcorp.com>
2017-03-07 11:34:16 +0900

    테스트 케이스 추가. index를 책의 Output에 나와있는것처럼 1부터 시작으로 변경

--- src/main/java/io/dktechin/jarvis/study/fence/Fence.java
+++ src/main/java/io/dktechin/jarvis/study/fence/Fence.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Fence {
 
-    private List<Integer> solve(double[] xi, double[] hi) {
+    public List<Integer> solve(double[] xi, double[] hi) {
         if ( xi.length != hi.length ) {
             throw new IllegalArgumentException("x와 h 길이가 다릅니다.");
         }
@@ -42,26 +42,15 @@ public class Fence {
         List<Integer> courses = new ArrayList<>();
         for ( int idx = 0; idx < course.length; idx++ ) {
             if ( course[ idx ] >= 0 ) {
-                courses.add(course[ idx ]);
+                courses.add(course[ idx ] + 1);
             }
         }
-        courses.add(course.length - 1);
+        courses.add(course.length);
 
         return courses;
     }
 
     private Double degree(int index, int target, double[] xi, double[] hi) {
         return (hi[ target ] - hi[ index ]) / (xi[ target ] - xi[ index ]);
-    }
-
-    public static final void main(String[] args) {
-        Fence fence = new Fence();
-        double[] xi = new double[] { 3, 6, 8, 12, 17, 20, 26, 28, 32 };
-        double[] hi = new double[] { 10, 5, 13, 6, 4, 10, 17, 14, 4 };
-        List<Integer> course = fence.solve(xi, hi);
-        for ( Integer index : course ) {
-            System.out.print(index + " ");
-        }
-        System.out.println();
     }
 }
