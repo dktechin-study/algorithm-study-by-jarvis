a992a49ac830512200e059ed148d620d591e2e97
jarvis <jarvis.kim88@gmail.com>
2018-02-19 01:01:04 +0900

    - 메일 프로그래밍   : LIS (DP로 함.)

---
+++ src/test/java/io/dktechin/jarvis/algo/mailprogramming/LISTest.java
@@ -0,0 +1,16 @@
+package io.dktechin.jarvis.algo.mailprogramming;
+
+import io.dktechin.jarvis.algo.mailprogrammaing.LIS;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LISTest {
+
+    @Test
+    public void test() {
+        assertEquals(3, LIS.solve(10, 9, 0, 7, 2, 5));
+        assertEquals(6, LIS.solve(0, 8, 12, 2,6,14, 9, 5, 13, 3, 11, 7, 15));
+        assertEquals(6, LIS.solve(10, 12, 14, 0, 16, 13, 14, 15, 16));
+    }
+}
