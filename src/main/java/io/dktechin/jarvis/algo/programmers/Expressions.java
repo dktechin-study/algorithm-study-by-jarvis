45601582df91085d141955a43058d8e6a9180045
jarvis <jarvis.kim88@gmail.com>
2018-02-19 18:37:50 +0900

    - 숫자의 표현  다른 사람 풀이 추가.

--- src/main/java/io/dktechin/jarvis/algo/programmers/Expressions.java
+++ src/main/java/io/dktechin/jarvis/algo/programmers/Expressions.java
@@ -14,6 +14,21 @@ package io.dktechin.jarvis.algo.programmers;
  */
 public class Expressions {
 
+    /**
+     * 다른 사람 풀이
+     * 댓글중
+     * 주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 홀수 약수의 개수와 같다라는 정수론 정리가 있습니다. ― 박동규 2018.2.7 20:51
+     */
+    public int expressions2(int num) {
+        int answer = 0;
+        for (int i = 1; i <= num; i += 2) {
+            if (num % i == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
     public int expressions(int num) {
         int answer = 0;
 
