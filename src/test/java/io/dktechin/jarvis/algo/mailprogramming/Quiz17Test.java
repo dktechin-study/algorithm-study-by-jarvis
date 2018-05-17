dd46aded219f9adf482f07a857fad169c4767438
jarvis-kim <korean4788@gmail.com>
2018-05-17 21:56:20 +0900

    실패 테이스 케이스 추가.

--- src/test/java/io/dktechin/jarvis/algo/mailprogramming/Quiz17Test.java
+++ src/test/java/io/dktechin/jarvis/algo/mailprogramming/Quiz17Test.java
@@ -20,6 +20,17 @@ public class Quiz17Test {
         // @formatter:on
         int expectedCase_1 = 8;
 
+        int[][] case_2 =
+                {{1, 0, 0, 1, 1, 0},
+                        {1, 0, 0, 1, 0, 0},
+                        {1, 1, 1, 1, 0, 0},
+                        {1, 0, 0, 0, 0, 1},
+                        {1, 1, 1, 1, 1, 1}};
+        // @formatter:on
+        int expectedCase_2 = -1;
+
+
         assertEquals(expectedCase_1, solve(case_1, new Pos(0, 0), new Pos(4, 0)));
+        assertEquals(expectedCase_2, solve(case_2, new Pos(0, 0), new Pos(5, 0)));
     }
 }
