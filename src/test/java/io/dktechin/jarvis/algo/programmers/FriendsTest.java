7c24db8d4a4fce10baaf57c45bf933d26aeb6a16
jarvis-kim <korean4788@gmail.com>
2018-05-23 17:10:12 +0900

    프로그래머스 : 카카오 프렌즈 컬러링북  - https://programmers.co.kr/learn/courses/30/lessons/1829?language=java

---
+++ src/test/java/io/dktechin/jarvis/algo/programmers/FriendsTest.java
@@ -0,0 +1,47 @@
+package io.dktechin.jarvis.algo.programmers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class FriendsTest {
+
+    @Test
+    public void testSolution() {
+        Friends.Solution solution = new Friends.Solution();
+
+        int case_1_input_m = 13;
+        int case_1_input_n = 16;
+        int[][] case_1_input_picture = {
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 3, 3, 3, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 0},
+                {0, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 0},
+                {0, 0, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 0, 0},
+                {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+        };
+        int[] case_1_expected = {12, 120};
+
+        int case_2_input_m = 6;
+        int case_2_input_n = 4;
+        int[][] case_2_input_picture = {
+                {1, 1, 1, 0},
+                {1, 1, 1, 0},
+                {0, 0, 0, 1},
+                {0, 0, 0, 1},
+                {0, 0, 0, 1},
+                {0, 0, 0, 1}
+        };
+        int[] case_2_expected = {2, 6};
+
+        assertArrayEquals(case_1_expected, solution.solution(case_1_input_m, case_1_input_n, case_1_input_picture));
+        assertArrayEquals(case_2_expected, solution.solution(case_2_input_m, case_2_input_n, case_2_input_picture));
+    }
+}
